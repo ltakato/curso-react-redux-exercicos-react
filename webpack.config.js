a2d301e86aa59ecb4c23cbaffd7a4598ea93d568
takato <leonardo.takato@soaexpert.com.br>
2019-02-27 09:26:37 -0300

    chore: setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './ex/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: './bundle.js'
+  },
+  devServer: {
+    port: 8000,
+    contentBase: './public'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
